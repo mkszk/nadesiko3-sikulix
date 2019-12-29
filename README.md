c865a90be17f62df6bd0e1e21d4441f357052730
m_k_szk <masaru.k.suzuki@gmail.com>
2019-12-30 00:19:15 +0900

    Import the repository to github.

---
+++ README.md
@@ -0,0 +1,31 @@
+# nadesiko3-sikulix
+
+nadesiko3-sikulix API bridge.
+
+## Prerequisites
+
+Download the sikulixapi-2.0.0.jar and place it as following,
+
+```
++ workdir/
+   + node_modules/
+   |
+   + sikulixapi-2.0.0.jar
+```
+
+### Installing
+
+Install by npm as following,
+
+```
+npm install https://github.com/mkszk/nadesiko3-sikulix.git
+```
+
+## Authors
+
+* **m_k_szk** - [mkszk](https://github.com/mkszk/)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
+
---
+++ index.js
@@ -0,0 +1,72 @@
+/**
+ * なでしこ3 プラグイン
+ * nadesiko3-sikulix
+ * SikuliXを呼び出すためのプラグイン
+ */
+const PluginSikuliX = {
+  '初期化': {
+    type: 'func',
+    josi: [],
+    fn: function (sys) {
+      // ここにプラグインの初期化処理
+      sys.__varslist[0]['バージョン'] = '0.0.1';
+      
+      var java = require('java');
+      sys.__varslist[0]['java'] = java;
+      java.classpath.push("sikulixapi-2.0.0.jar");
+      java.import('org.sikuli.script.ImagePath').add(".")
+    }
+  },
+  
+  'マウス移動': {
+    type: 'func',
+    josi: [['へ']],
+    fn: function (target, sys) {
+      var screen = sys.__varslist[0]['java'].newInstanceSync('org.sikuli.script.Screen');
+      if (target['x'] != undefined && target['y'] != undefined) {
+        screen.mouseMoveSync(target['x'], target['y']);
+      } else {
+        screen.mouseMoveSync(target);
+      }
+      return screen;
+    }
+  },
+  
+  'クリック': {
+    type: 'func',
+    josi: [['を']],
+    fn: function (target, sys) {
+      target.clickSync();
+      return;
+    }
+  },
+  
+  'ダブルクリック': {
+    type: 'func',
+    josi: [['を']],
+    fn: function (target, sys) {
+      target.doubleClickSync();
+      return;
+    }
+  },
+  
+  '画面取得': {
+    type: 'func',
+    josi: [],
+    fn: function (sys) {
+      return sys.__varslist[0]['java'].newInstanceSync('org.sikuli.script.Screen');
+    }
+  },
+  
+  '画面探索': {
+    type: 'func',
+    josi: [['から'], ['を']],
+    fn: function (screen, target, sys) {
+      return screen.findSync(target);
+    }
+  },
+}
+
+// モジュールのエクスポート(必ず必要)
+module.exports = PluginSikuliX
+
